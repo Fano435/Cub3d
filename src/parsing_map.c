e093446368dccc9320037f5bed3c64f26e2e97cc
Edenbdd <aubertra@student.42.fr>
2025-02-18 11:34:08 +0100

    file to array to test + final map parse to code

--- src/parsing_map.c
+++ src/parsing_map.c
@@ -6,7 +6,7 @@
 /*   By: aubertra <aubertra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 18:16:25 by aubertra          #+#    #+#             */
-/*   Updated: 2025/02/18 11:17:41 by aubertra         ###   ########.fr       */
+/*   Updated: 2025/02/18 11:33:26 by aubertra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ int get_len(char *line, int max_len)
         return (max_len);
 }
 
-int get_height(int fd_config)
+int get_height(int fd_config, char *config_file)
 {
     int     height;
     int     ret_map;
@@ -64,6 +64,8 @@ int get_height(int fd_config)
 
     height = 0;
     max_len = 0;
+    close(fd_config);
+    fd_config = open(config_file, O_RDONLY);
     while (1)
     {
         ret_map = 0;
@@ -76,24 +78,96 @@ int get_height(int fd_config)
         else if (ret_map == -1)
             return (-1);
         max_len = get_len(line, max_len);
-        dprintf(STDERR_FILENO, "len is %d\n", max_len);
         free(line);
     }
     return (height);
 }
 
+int get_len2(int fd_config, char *config_file)
+{
+    int     height;
+    int     ret_map;
+    char    *line;
+    int     max_len;
+
+    height = 0;
+    max_len = 0;
+    close(fd_config);
+    fd_config = open(config_file, O_RDONLY);
+    while (1)
+    {
+        ret_map = 0;
+        line = get_next_line(fd_config);
+        if (!line)
+            break; 
+        ret_map = is_map(line);
+        if (ret_map == 1)
+            height++;
+        else if (ret_map == -1)
+            return (-1);
+        max_len = get_len(line, max_len);
+        free(line);
+    }
+    return (max_len);
+}
+
+char **file_to_array(int fd_config, char *config_file, int len, int height)
+{
+    char    **map;
+    char    *line;
+    int     i;
+    int     j;
+    int     pos;
+
+    close(fd_config);
+    fd_config = open(config_file, O_RDONLY);
+    map = (char **)malloc(sizeof(char *) * (height + 1));
+    if (!map)
+        return (error_msg(6), NULL);
+    i = 0;
+    while (1)
+    {
+        line = get_next_line(fd_config);
+        if (!line)
+            break; 
+        if (is_map(line) == 1)
+        {
+            pos = 0;
+            map[i] = (char *)malloc(sizeof(char) * (len + 1));
+            if (!map[i])
+                return (error_msg(6), NULL);
+            j = 0;
+            while (line[pos])
+            {
+                map[i][j] = line[pos];
+                j++;
+                pos++;
+            }
+            map[i][j] = '\0';
+        }
+    }
+    map[i] = NULL;
+    return (map);
+}
+
 int parse_map(char *config_file, int done, int fd_config)
 {
-    // char    **map;
+    char    **map;
     int     height;
-    
+    int     len;
+
     if (done != 6)
         return (error_msg(4));
     close(fd_config);
     fd_config = open(config_file, O_RDONLY);
-    height = get_height(fd_config);
-    if (height < 1)
+    height = get_height(fd_config, config_file);
+    len = get_len2(fd_config, config_file);
+    dprintf(STDERR_FILENO, "len is %d\n", len);
+    dprintf(STDERR_FILENO, "height is %d\n", height);
+    if (height < 1 || len < 1)
         return (-1);
+    map = file_to_array(fd_config, config_file, len, height);
+    // print_map();
     done++;
     return (0);
 }
